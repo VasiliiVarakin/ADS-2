a940a2e38b80ae418a9125a35c99e8f300d564f8
VasiliiVarakin <62684963+VasiliiVarakin@users.noreply.github.com>
2020-06-01 22:04:31 +0300

    Update alg2.cpp

--- src/alg2.cpp
+++ src/alg2.cpp
@@ -1,43 +1,43 @@
 int countPairs1(int* arr, int len, int value){
-int k = 0;
+int n = 0;
 	for (int i = 0; i < len-1; i++){
 		for (int j = i + 1; j < len; j++){
 			if (arr[i] + arr[j] == value)
-			k++;
+			n++;
 		}
 	}
-	return k;
+	return n;
 }
 
 int countPairs2(int* arr, int len, int value){
-	int k = 0;
+	int n = 0;
 		for (int i = 0; i < len - 1; i++){
 			for (int j = len - 1; j > i; j--){
 				if (arr[i] + arr[j] == value){
-				k++;
+				n++;
 				}
 			}
 		}
-	return k;
+	return n;
 }
 
 int countPairs3(int* arr, int len, int value){
-	int k = 0;
+	int n = 0;
 	for (int i = 0; i < len - 1; i++){
 		int left = i, right = len;
 
 			while (left < right-1){
 				int mid = (left + right) / 2;
 				if (arr[i] + arr[mid] == value){
-				k++;
+				n++;
 				int j = mid+1;
 			while (arr[i] + arr[j] == value && j <right){
-			k++;
+			n++;
 			j++;
 			}
 			j = mid - 1;
 			while (arr[i] + arr[j] == value && j>left){
-				k++;
+				n++;
 				j--;
 			}
 			break;
@@ -48,5 +48,5 @@ int countPairs3(int* arr, int len, int value){
 		left = mid;
 		}
 	}
-return k;
+return n;
 }
